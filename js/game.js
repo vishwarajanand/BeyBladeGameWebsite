707eabcaffd0467f74cc96396fe066eefb63bdd8
Vishwaraj Anand <vishwarajanand@fb.com>
2021-09-11 00:12:15 +0530

    Untested: Basic board creation in canvas

--- js/game.js
+++ js/game.js
@@ -1,2 +1,13 @@
 window.onload = function () {
+  var c = document.getElementById("game");
+  var ctx = c.getContext("2d");
+  // var img = document.getElementById("img/bb_board.png");
+  var img = new Image();
+  img.src = "img/bb_board.png";
+  img.onload = function () {
+    ctx.drawImage(img, 2, 2);
+    console.log("the image is drawn");
+  };
+
+  ctx.drawImage(img, 1, 1, 1000, 1000);
 };
